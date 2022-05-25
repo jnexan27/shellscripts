73e468228701e0d31d4941e40e20128cc52236c9
Jesús Andrés Manzanilla Nexan <jnexan20@gmail.com>
2022-05-25 08:50:43 -0400

    Delete scripts/permisos_asterisk directory

--- scripts/permisos_asterisk/permisos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-chown -R telefonia.telefonia /usr/lib64/asterisk/
-chown -R telefonia.telefonia /var/lib/asterisk/
-chown -R telefonia.telefonia /var/spool/asterisk/
-chown -R telefonia.telefonia /var/log/asterisk/
-chown -R telefonia.telefonia /var/run/asterisk/
-chown -R telefonia.telefonia /usr/sbin/asterisk
